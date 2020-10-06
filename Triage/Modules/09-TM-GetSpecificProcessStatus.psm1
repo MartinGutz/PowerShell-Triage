108dde38362b24bbc11be3f17c713efc36590cdc
Martin Gutierrez <martingutz@gmail.com>
2020-10-05 20:30:51 -0500

    Added a module to check for specific application processes

---
+++ Triage/Modules/09-TM-GetSpecificProcessStatus.psm1
@@ -0,0 +1,83 @@
+function Get-TName()
+{
+    return "Checking Application Specific Processes"
+}
+
+function Get-TData()
+{
+	$processesToCheck = @()
+	$processesToCheck += "Notepad"
+	$processesToCheck += "OneDrive"
+	
+	$processList = @()
+	$processes = Get-Process
+	
+	foreach($processName in $processesToCheck)
+	{
+		if(($processes | Where-Object {$_.Name -contains $processName}))
+		{
+			$foundProcess = ($processes | Where-Object {$_.Name -contains $processName})
+			$foundProcessItem =[pscustomobject]@{
+				name = $processName
+				data = $foundProcess
+				isFound = $true
+			}
+			$processList += $foundProcessItem
+		}else {
+			$foundProcessItem =[pscustomobject]@{
+				name = $processName
+				data = ""
+				isFound = $false
+			}
+			$processList += $foundProcessItem
+		}
+	}
+
+	$returnObject =[pscustomobject]@{
+		data = $processList
+	}
+	return $returnObject
+}
+
+function Get-TReportData($objects)
+{
+	$report = "Process List: "
+	foreach($object in $objects.data)
+	{
+		$report += "`n Process Name: "
+		$report += $object.Name
+		if ($object.isFound) {
+			$report += "`n Process Status: Running"
+			$report += "`n Process ID: "
+			$report += $object.data.Id
+		}else{
+			$report += "`n Process Status: Not Found"
+			$report += "`n Process ID: -"
+		}
+	}
+	return $report
+}
+
+function Get-THTMLData($objects)
+{
+	$report = "<b>Status of Application Processes: </b>"
+	foreach($object in $objects.data)
+	{
+		$report += "</br><b>Process Name: </b>"
+		$report += $object.Name
+		if ($object.isFound) {
+			$report += "</br><b>Process Status: </b>Running"
+			$report += "</br><b>Process ID: </b>"
+			$report += $object.data.Id
+		}else{
+			$report += "</br><b>Process Status: </b>Not Found "
+			$report += "</br><b>Process ID: </b>-"
+		}
+	}
+	return $report
+}
+
+Export-ModuleMember -Function Get-TName
+Export-ModuleMember -Function Get-TData
+Export-ModuleMember -function Get-TReportData
+Export-ModuleMember -function Get-THTMLData
